6630ce929e1547b276a12800757ba9f638da23bd
Cgeorge1807 <christian03george@gmail.com>
2024-12-20 19:20:46 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -4,8 +4,19 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let domains = [".com", ".net", ".org", ".io"];
+
+  let randomPronoun = pronouns[Math.floor(Math.random() * pronouns.length)];
+  let randomAdjective =
+    adjectives[Math.floor(Math.random() * adjectives.length)];
+  let randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+  let randomDomain = domains[Math.floor(Math.random() * domains.length)];
+
+  let randomDomainName = `${randomPronoun}${randomAdjective}${randomNoun}${randomDomain}`;
+
+  document.getElementById("domain").innerText = randomDomainName;
 };
